ca28cb1e283ddec5a1b9592b2aac320ff5eedc76
blipp <bclipp770@gmail.com>
2021-05-21 10:07:44 -0400

    updating handler

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,13 @@
-import sys
+import pickle5 as pickle
+import sklearn
+import numpy as np
 
 
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'
+    loaded_model = pickle.load(open("model.pkl", 'rb'))
+    X = np.array([event]).reshape(-1, 1)
+    prediction = loaded_model.predict(X)
+    return {
+        'statusCode': 200,
+        'body': prediction
+    }
